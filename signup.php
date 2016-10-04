90caf65caca6e099434683697b8401f146fac0a6
subdime <sc.markos@gmail.com>
2016-10-04 22:32:01 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,22 +1,52 @@
 <?php
-	session_start();
-?>
-<?php
 	include 'connectdb.php';
 	
 	$usname=$_POST['Username'];
 	$psd=$_POST['Password'];
 	
-	$res=mysqli_query($conn,"SELECT Username FROM users  WHERE Username='$usname'");
+	if (isset($_POST['signup_button'])){
+		if (strlen($usname)<3){ 
+			header('Location: fileziller.php?msg=1');
+			exit;
+		}
+		else if (strlen($psd)<3){
+			header('Location: fileziller.php?msg=2');
+			exit;
+		}
+		$res=mysqli_query($conn,"SELECT Username FROM users  WHERE Username='$usname'");
+		
+		if ( mysqli_num_rows($res)<1){
+			mysqli_query($conn,"INSERT INTO users (Username, Password) VALUES ('$usname','$psd')") or die(mysqli_error());
+			$_SESSION["Username"]=$usname;
+		}
+		else{
+			header("Location: fileziller.php?msg=3");
+			exit;
+		}
 	
-	if ( mysqli_num_rows($res)<1){
-		 mysqli_query($conn,"INSERT INTO users (Username, Password) VALUES ('$usname','$psd')") or die(mysqli_error());
-		 $_SESSION["Username"]=$usname;
-	}
-	else{
 		header("Location: fileziller.php");
 	}
-	
-	header("Location: fileziller.php");
-	
+	else if (isset($_POST['login_button'])){
+		$res=mysqli_query($conn,"SELECT Username,Password FROM users  WHERE Username='$usname'");
+		//if username exists
+		if ( mysqli_num_rows($res)>0){
+			$row=mysqli_fetch_array($res,MYSQLI_ASSOC);
+			//if psd is correct
+			if ($row['Password']==$psd){
+				$_SESSION["Username"]=$usname;
+				header("Location: fileziller.php");
+				exit;
+			}
+			else{
+				header('Location:fileziller.php?msg=4');
+				exit;
+			}
+		}
+		else{
+			header('Location:fileziller.php?msg=5');
+			// no user with this username;
+		}
+	}
+	else
+		header('Location:fileziller.php?msg=6')
 ?>
